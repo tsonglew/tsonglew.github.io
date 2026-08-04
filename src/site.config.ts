478c05402dacafc96c46b762da707ea2a046cff7
Tsonglew <tsonglew@gmail.com>
2026-08-04 17:20:57 +0800

    feat: use GitHub avatar as site favicon

--- src/site.config.ts
+++ src/site.config.ts
@@ -9,7 +9,7 @@ export const theme: ThemeUserConfig = {
   /** Description metadata for your website. Can be used in page metadata. */
   description: "Tsonglew's Blog",
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
-  favicon: '/favicon/favicon.svg',
+  favicon: '/favicon/favicon-32x32.png',
   /** The default social card image for your site which should be a path to an image in the `public/` directory. */
   socialCard: '/images/social-card.png',
   /** Specify the default language for this site. */
